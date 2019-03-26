35d01891999ed71a1cd737d4c693f7b97ae55401
Ralph Mueller <ralph.mueller.de@gmail.com>
2019-03-26 12:59:41 +0100

    datumsformat jetzt aud DE

--- static/js/lookup_besucher.js
+++ static/js/lookup_besucher.js
@@ -9,7 +9,7 @@ function getBuchungenForBesucher(t, h3Text, stat){
 				.show()
 				.append($('<h3>').text(h3Text))
             	.append ($.map(data, function (v) {return $('<p>').html(
-  			  		`<span class="badge badge-dark">${v.id}</span>  ${v.apartment.name} ${v.anreise} ${v.abreise} ${v.miete} ${v.kurtaxe} ${v.summe}` 
+  			  		`<span class="badge badge-dark">${v.id}</span>  ${v.apartment.name} ${new Date(v.anreise).toLocaleDateString()}-${new Date(v.abreise).toLocaleDateString()} ${v.miete} ${v.kurtaxe} ${v.summe}` 
   			  	);
   		  	}))
 		}
