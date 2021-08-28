93d8c08a2d8b3051f8100281041b5e6946b64835
Ralph Mueller <ralph.mueller.de@gmail.com>
2021-08-28 09:03:51 +0200

    reorh ok now

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -12,45 +12,43 @@ import datetime
 from flask import Flask, render_template
 from flask_cors import CORS
 
-sys.path.append('../libs') # to include file fewo_reporting
+# sys.path.append('../../libs') # to include file fewo_reporting
+import bkormlib
+bkormlib.envir = 'development'
     
+    
+app = Flask(__name__, instance_relative_config=True)
+
+from . import home
+app.register_blueprint(home.bp)
+ 
+@app.route("/resetinvoice/")
+def reset_invoice_new():
+    return render_template('reset_invoice_new.html', run_mode=bkormlib.envir) 
+
+CORS(app)
+app.secret_key = 'google hupf schmeckt gut'
+
+# import auth
+# app.register_blueprint(auth.bp)
+
+from . import besucher
+app.register_blueprint(besucher.bp)
+
+from . import buchung
+app.register_blueprint(buchung.bp)
+
+from . import rest
+app.register_blueprint(rest.bp)
+
+# from . import stats
+# app.register_blueprint(stats.bp)
+
+from . import fewocalendar
+app.register_blueprint(fewocalendar.bp)
+
+from . import json_routes
+app.register_blueprint(json_routes.bp)
+
 if __name__ == "__main__":
-    
-    app = Flask(__name__)
-
-    import bkormlib
-    bkormlib.envir = 'development'
-
-    @app.route("/")
-    def home():
-        return render_template('index.html', title="Willkommen", run_mode=bkormlib.envir)
-     
-    @app.route("/resetinvoice/")
-    def reset_invoice_new():
-        return render_template('reset_invoice_new.html', run_mode=bkormlib.envir) 
-    
-    CORS(app)
-    app.secret_key = 'google hupf schmeckt gut'
-    
-    # import auth
-    # app.register_blueprint(auth.bp)
-    
-    import besucher
-    app.register_blueprint(besucher.bp)
-
-    import buchung
-    app.register_blueprint(buchung.bp)
-    
-    import rest
-    app.register_blueprint(rest.bp)
-    
-    import stats
-    app.register_blueprint(stats.bp)
-    
-    import fewocalendar
-    app.register_blueprint(fewocalendar.bp)
-    
-    import json_routes
-    app.register_blueprint(json_routes.bp)
-    
     app.run(debug=True, port=5000)
---
+++ flaskr/home.py
@@ -0,0 +1,18 @@
+'''
+Created on 20 March 2019
+
+@author: ralph
+
+find or create besucher
+
+'''
+from flask import Blueprint, render_template, request
+
+from bkormlib import envir
+
+bp = Blueprint('home', __name__, url_prefix='/')
+
+@bp.route('/')
+def home():
+    return render_template('index.html',
+        run_mode=envir)
