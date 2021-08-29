d0290c35f932b57b5847f2d3b9f5351325de5d3b
Ralph Mueller <ralph.mueller.de@gmail.com>
2021-08-29 14:42:34 +0200

    nav mit rudimentärem login/logout

--- flaskr/besucher.py
+++ flaskr/besucher.py
@@ -6,11 +6,23 @@ Created on 20 March 2019
 find or create besucher
 
 '''
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, flash, redirect, url_for
 
-from bkormlib import envir
+from bkormlib import envir, Besucher
 
 bp = Blueprint('besucher', __name__, url_prefix='/besucher')
+
+@bp.route('/')
+def besucher():
+    query = (Besucher
+        .select()
+        .order_by(Besucher.name)
+    )
+    if len(list(query)) > 0:
+        return render_template('besucher/index.html', besucher=query)
+    else:
+        flash('no visitors found')
+        return(redirect(url_for('home.index')))
 
 @bp.route('/find')
 def besucher_find():
--- flaskr/buchung.py
+++ flaskr/buchung.py
@@ -20,13 +20,13 @@ def buchung(id):
         print('POST')
         print(request.form)
         flash('Supi!')
-        return(redirect(url_for('home')))
+        return(redirect(url_for('home.index')))
     else:
         buchung = Buchung.get(Buchung.id == id)
         if buchung.status in ['abgerechnet', 'storno', 'verworfen']:
             print('Buchung {} mit Status {} kann nicht geändert werden!'.format(buchung.id, buchung.status))
             flash('Buchung {} mit Status {} kann nicht geändert werden!'.format(buchung.id, buchung.status), 'error')
-            return(redirect(url_for('home')))
+            return(redirect(url_for('home.index')))
         besucher = buchung.besucher
         return render_template('buchung_display.html', 
             buchung=buchung,
