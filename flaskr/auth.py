888bd18bc0f32e7230817ee65d826399a6d3cf4d
Ralph Mueller <ralph.mueller.de@gmail.com>
2021-08-29 11:13:53 +0200

    auth change

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -80,6 +80,7 @@ def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
+            flash('Login Required')
             return redirect(url_for('auth.login'))
 
         return view(**kwargs)
