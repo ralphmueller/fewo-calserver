d2746b0a51b41755e99a9e52da1c6fe7bef1f336
Ralph Mueller <ralph.mueller.de@gmail.com>
2022-09-26 12:45:20 +0200

    Später Transfer: Keine Vorauszahlung Email mehr an den Besucher

--- flaskr/utils/api.py
+++ flaskr/utils/api.py
@@ -5,6 +5,7 @@ API to models and helper functions
 
 @author: ralph
 '''
+from datetime import date
 from peewee import fn
 from bkormlib import Besucher, Buchung, Apartment, Email, User
 
@@ -401,8 +402,10 @@ def send_angebot_emails(buchung, email_text):
 
 def send_update_emails(buchung, buchung_alt, res):
     # send emails
-    if 'vorauszahlung' in res:
+    if 'vorauszahlung' in res and buchung.anreise <= date.today():
         # send payment confirmation to besucher
+        # 26.10.2022: Only send if is really a prepayment, and not if it is 
+        #   just a transfer after the visit
         email_html = render_template(
             'emails/{}/buchung_vorauszahlung.html'
             .format(buchung.besucher.language.lower()),
